8bb5628719210d4b273a2535a7dca93e291645dd
rohan <rohan.hart@fronde.com>
2017-09-26 11:25:51 +1300

    migrate to dotnet core 1.1

--- src/CommonLibrariesForNET/ServiceHttpClient.cs
+++ src/CommonLibrariesForNET/ServiceHttpClient.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Salesforce.Common.Models;
+using Salesforce.Common.Models.Json;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Salesforce.Common.Serializer;
